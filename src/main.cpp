24ece47b6c7522698c0761e494fa8e7e1e378974
Joachim Kittelberger <jkittelberger@kibesoft.de>
2023-07-28 13:01:08 +0200

    Fix intellisense Problems wtih ESP8266 and comment libraries

--- src/main.cpp
+++ src/main.cpp
@@ -138,8 +138,8 @@ KSFTPServer ftp;
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #elif defined(ESP8266)
-#include <ESP8266WiFi.h>
-#include <ESPAsyncTCP.h>
+//#include <ESP8266WiFi.h>
+//#include <ESPAsyncTCP.h>
 #endif
 #include <ESPAsyncWebServer.h>
 AsyncWebServer server(80);
