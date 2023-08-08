935dd24a523310cc924e985e5140c5e8930cf3c4
Joachim Kittelberger <jkittelberger@kibesoft.de>
2023-08-08 16:10:25 +0200

    Use KSESPFramework

--- src/main.cpp
+++ src/main.cpp
@@ -31,20 +31,27 @@
 
 /**
  * @copyright
- * Copyright (C) 2022, KibeSoft - Joachim Kittelberger, (https://www.kibesoft.de)
- * All rights reserved
+ * MIT License
+ *
+ * Copyright (c) 2023 Joachim Kittelberger - KibeSoft, www.kibesoft.de
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
  * 
- *     https://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 
@@ -66,14 +73,15 @@
 #include <Arduino.h>
 #include <Wire.h>
 
-
-#include "KSESPFrameworkSettings.h"
-#include "KSCredentials.h"
+// Einbindung KSESPFramework
+//#include "KSESPFrameworkSettings.h"		// funktioniert nicht. Wir müssen die platformio.ini-Settings dazu nutzen
+#include "KSESPFramework.h"
+#include "../../../ESP32/KSESPLibraries/KSESPFramework/include/KSCredentials.h"			 // use global version of credentials
+//#include "myCredentials.h"             // use local version of credentials. must bei copied from KSCredentials.h.tpl
 
 
 #include "AppInfo.h"
 #include "AppCredentials.h"			// global App Credentials: must be copied from AppCredentials.h.tpl
-//#include "KSCredentials.h"			// global KSLibrariesCredentials: must bei copied from KSCredentials.h.tpl
 
 #include "JSONMessage.h"
 
